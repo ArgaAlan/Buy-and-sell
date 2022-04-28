11724d32056e8c847f1deee5a68e9f01221d050a
Alan Gonzalez <argaalan@hotmail.com>
2022-04-28 11:05:53 -0600

    first component

---
+++ src/app/listings-pag/listings-pag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-pag',
+  templateUrl: './listings-pag.component.html',
+  styleUrls: ['./listings-pag.component.css']
+})
+export class ListingsPagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
