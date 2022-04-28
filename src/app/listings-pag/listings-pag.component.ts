57f26694ac0c8497a23654d99c69cd4cfb690047
Alan Gonzalez <argaalan@hotmail.com>
2022-04-28 11:34:37 -0600

    listings page

--- src/app/listings-pag/listings-pag.component.ts
+++ src/app/listings-pag/listings-pag.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from "../types";
+import { fakeListings } from "../fake-data";
 
 @Component({
   selector: 'app-listings-pag',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listings-pag.component.css']
 })
 export class ListingsPagComponent implements OnInit {
+  listings: Listing[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+     this.listings = fakeListings;
   }
 
 }
